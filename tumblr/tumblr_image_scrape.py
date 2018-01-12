f10b9e466cbe277ac0ebd7e03ee5e00c2c688f72
joaoDragado <dubmecoco@yahoo.com>
2018-01-12 11:15:44 +0200

    added generic tumblr script

---
+++ tumblr/tumblr_image_scrape.py
@@ -0,0 +1,45 @@
+import requests, bs4
+
+'''
+script that scrapes the links to all the images on the nameOfSite tumblr, and :
+- saves it to a text file, 
+- downloads the image in a folder named "images". 
+
+Saving the links before downloading is advisable, to check and filter out avatar & non-pertinent images.
+
+'''
+
+images = []
+
+base_url = 'https://nameOfSite.tumblr.com/page/{}'
+
+for i in range(50):
+    
+    url = base_url.format(i)
+    r = requests.get(url)
+    try:
+        r.raise_for_status()
+    except Exception as exc:
+        print('There was a problem : {}'.format(exc))
+    
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    origin = [x['src'] for x in soup.findAll('img') ]
+    images.extend(origin)    
+
+'''Option 1 - Save the web links to the images to a text file
+Uncomment below code to activate
+'''
+
+#with open('nameOfSite.txt', mode='at', encoding='utf-8') as myfile:
+#    myfile.write('\n'.join(images))
+
+
+'''Option 2 - Save the images to ./images
+Uncomment below code to activate
+'''
+
+#for img_url in images:
+#    img = img_url.split('/')[-1]
+#    with open(os.path.join('images', os.path.basename(img)), 'wb') as image_file:
+#        for tranche in r.iter_content(10000):
+#            image_file.write(tranche)
