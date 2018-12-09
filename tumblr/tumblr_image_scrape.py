f73243e4205702d365799b7a698aefd1a344beb5
joaoDragado <dubmecoco@yahoo.com>
2018-12-09 22:32:49 +0200

    added scaping posts to tumblr script.

--- tumblr/tumblr_image_scrape.py
+++ tumblr/tumblr_image_scrape.py
@@ -2,29 +2,47 @@ import requests, bs4
 
 '''
 script that scrapes the links to all the images on the nameOfSite tumblr, and :
-- saves it to a text file, 
-- downloads the image in a folder named "images". 
+- saves it to a text file,
+- downloads the image in a folder named "images".
 
 Saving the links before downloading is advisable, to check and filter out avatar & non-pertinent images.
 
 '''
 
 images = []
+posts = []
 
 base_url = 'https://nameOfSite.tumblr.com/page/{}'
 
 for i in range(50):
-    
+
     url = base_url.format(i)
     r = requests.get(url)
     try:
         r.raise_for_status()
     except Exception as exc:
         print('There was a problem : {}'.format(exc))
-    
+
     soup = bs4.BeautifulSoup(r.text, 'html.parser')
+
     origin = [x['src'] for x in soup.findAll('img') ]
-    images.extend(origin)    
+    images.extend(origin)
+
+    content = [x.text.strip() for x in soup.findAll('div', class_='post-body')]
+    posts.extend(content)
+
+### Posts (text)
+
+'''Save the content of each post to a text file
+Uncomment below code to activate
+'''
+
+#with open('nameOfSite_posts.txt', mode='at', encoding='utf-8') as myfile:
+#    myfile.write('\n'.join(posts))
+
+
+
+### Images
 
 '''Option 1 - Save the web links to the images to a text file
 Uncomment below code to activate
